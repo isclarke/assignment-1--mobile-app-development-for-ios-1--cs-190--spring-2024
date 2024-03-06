775dde2ac6cc3335f7d3d8f079e5060bdc399049
isclarke <156957402+isclarke@users.noreply.github.com>
2024-03-06 18:43:41 -0500

    add top of pyramid code
    I added a portion of the code that sees if the diamond size the user input is even or odd. Then I added a for loop that uses the input and creates white spaces.

--- main.swift
+++ main.swift
@@ -1,3 +1,8 @@
 print("Enter the size of your diamond")
 let diamondsize = Int(readLine()!)!
-let evennumber = %2 == 0
+let evennumber = diamondsize % 2 == 0
+for i in 1..<diamondsize - 1 {
+  for _ in 0..<diamondsize - 1  {
+    print(" ", terminator: " ")
+}
+}
