64b832fc183e46508268b65a9da2d1ea0d81d4f7
isclarke <156957402+isclarke@users.noreply.github.com>
2024-03-06 19:57:22 -0500

    restart
    The code I had was not really working, so I deleted it most of it, and left what worked.

--- main.swift
+++ main.swift
@@ -1,11 +1,3 @@
 print("Enter the size of your diamond")
-let diamondsize = Int(readLine()!)!
-let evennumber = diamondsize % 2 == 0
-for i in 1...diamondsize{
-  for i in 0..<diamondsize - 1  {
-    print(" ", terminator: " ")
-}
-for i in 1...(2 * i - 1){
-  print("*", terminator: " ")
-}
-print("")
+let diamondSize = Int(readLine()!)!
+let evennumber = diamondSize % 2 == 0
