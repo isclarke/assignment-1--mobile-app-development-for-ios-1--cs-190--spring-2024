a52f849630b924b64efe8be5af08c74990615df1
isclarke <156957402+isclarke@users.noreply.github.com>
2024-03-06 19:10:31 -0500

    add upper asterisks based on input
    here I attempted to add code that reads the input and adds the top part of the diamond pattern.

--- main.swift
+++ main.swift
@@ -1,8 +1,11 @@
 print("Enter the size of your diamond")
 let diamondsize = Int(readLine()!)!
 let evennumber = diamondsize % 2 == 0
-for i in 1..<diamondsize - 1 {
-  for _ in 0..<diamondsize - 1  {
+for i in 1...diamondsize{
+  for i in 0..<diamondsize - 1  {
     print(" ", terminator: " ")
 }
+for i in 1...(2 * i - 1){
+  print("*", terminator: " ")
 }
+print("")
