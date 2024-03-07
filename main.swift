9cd068e71e85a4190d0b13122aa383492b8e3205
isclarke <156957402+isclarke@users.noreply.github.com>
2024-03-06 20:17:21 -0500

    add code for upper part of diamond

--- main.swift
+++ main.swift
@@ -1,3 +1,11 @@
 print("Enter the size of your diamond")
 let diamondSize = Int(readLine()!)!
 let evennumber = diamondSize % 2 == 0
+for i in 1...diamondSize {
+  for _ in 0..<(diamondSize - i) {
+  }
+    print(" ",terminator: " ")
+  }
+for _ in 1...(2 * diamondSize - 1) {
+
+}
