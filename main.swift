99c1bc6a4fc93b5a05978f7ff8ec785b242984ca
isclarke <156957402+isclarke@users.noreply.github.com>
2024-03-18 15:35:17 -0400

    remove code

--- main.swift
+++ main.swift
@@ -1,12 +1,3 @@
 print("Enter the size of your diamond")
 let diamondSize = Int(readLine()!)!
 var evennumber = diamondSize % 2 == 0
-for i in 1...diamondSize {
-  for _ in 0..<(diamondSize - i) {
-  }
-    print(" ",terminator: " ")
-  }
-for _ in 1...(2 * diamondSize - 1) {
-  print("*", terminator: " ")
-}
-print(" ")
