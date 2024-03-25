21a4a8de7e3e01586b532aa35ce48390430805cb
isclarke <156957402+isclarke@users.noreply.github.com>
2024-03-25 19:20:04 -0400

    add forloop for even

--- main.swift
+++ main.swift
@@ -3,9 +3,32 @@ if let diamondSize = Int(readLine()!) {
 let evennumber = diamondSize % 2 == 0
 
 if evennumber {
-for _ in 1...diamondSize{
-  print(" ", terminator: "*")
+  for i in 0..<diamondSize / 2 + 1   {
+    let spaceCount : Int
+    let starCount : Int
+    spaceCount = diamondSize / 2 - i
+  starCount = 2 * (i + 1) - 2
+  for _ in 0..<spaceCount{
+      print(" ", terminator: " ")
+  }
+  for _ in 0..<starCount {
+      print(" ",terminator:"*")
+  }
+    print("")
 }
+  for i in 0..<diamondSize / 2{
+    let spaceCount : Int
+    let starCount : Int
+    spaceCount = i + 1
+    starCount = diamondSize - 2 * (i + 1)
+  for _ in 0..<spaceCount{
+    print(" ",terminator: " ")
+  }
+  for _ in 0..<starCount {
+    print(" ",terminator : "*")
+  }
+  print("")
+  }
 }
 else {
   for i in 0..<diamondSize / 2 + 1 {
